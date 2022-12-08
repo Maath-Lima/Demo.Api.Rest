9f084c5b09d943ea2e1c076894a6dc70c1438659
Matheus <math3226@gmail.com>
2022-12-08 14:08:33 -0300

    Conclusão da modelagem

---
+++ src/DevIO.Api/Configuration/ApiConfig.cs
@@ -0,0 +1,21 @@
+﻿namespace DevIO.Api.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IApplicationBuilder UseWebApiConfig(this IApplicationBuilder app)
+        {
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            return app;
+        }
+    }
+}
--- src/DevIO.Api/Program.cs
+++ src/DevIO.Api/Program.cs
@@ -35,10 +35,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseWebApiConfig();
 
 app.Run();
